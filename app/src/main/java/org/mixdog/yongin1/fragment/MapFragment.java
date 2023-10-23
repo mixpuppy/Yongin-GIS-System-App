81844e11718c7db0c64c41e25cc647ad7824da3c
mixdog <mlv0vlm@naver.com>
2023-10-23 09:41:03 +0900

    error

--- app/src/main/java/org/mixdog/yongin1/fragment/MapFragment.java
+++ app/src/main/java/org/mixdog/yongin1/fragment/MapFragment.java
@@ -85,10 +85,10 @@ public class MapFragment extends Fragment
 //    private final String noiseUrl = "http://172.30.1.55:8081/getNoise";
 //    private final String vibrationUrl = "http://172.30.1.55:8081/getVibration";
 
-    private final String xyUrl = "http://172.30.1.22:80/gis/temp/gps";
-    private final String noiseUrl = "http://172.30.1.22:80/gis/temp/noise";
-    private final String vibrationUrl = "http://172.30.1.22:80/gis/temp/rpm";
-    private final String requestCarNumUrl = "http://172.30.1.22:80/gis/car";
+    private final String xyUrl = "http://172.30.1.62:80/gis/temp/gps";
+    private final String noiseUrl = "http://172.30.1.62:80/gis/temp/noise";
+    private final String vibrationUrl = "http://172.30.1.62:80/gis/temp/rpm";
+    private final String requestCarNumUrl = "http://172.30.1.62:80/gis/car";
     private final String startSendUrl = "http://172.30.1.61:80/gis/start";
     private final String stopSendUrl = "http://172.30.1.61:80/gis/stop";
 
